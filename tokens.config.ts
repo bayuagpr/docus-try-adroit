7607784f0f0e7a15b944cfd6066b96a3e87899a6
Bayu Prakoso <39822743+bayuagpr@users.noreply.github.com>
2023-11-01 15:58:49 +0700

    feat: color primary

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,4 +1,20 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
+  color: {
+    primary: {
+      '0': '#F1FCFF',
+      '1': '#DCF7FF',
+      '2': '#C5F2FF',
+      '3': '#82E3FF',
+      '4': '#55E1FF',
+      '5': '#1AD6FF',
+      '6': '#09A0C1',
+      '7': '#024757',
+      '8': '#00232B',
+      '9': '#001A1F',
+      '10': '#DCF7FD',
+      '11': '#FDF2EB'
+    }
+  }
 })
